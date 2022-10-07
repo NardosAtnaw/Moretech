cfe2d4721776f9256478ada1b99712bcd16e9418
nardos atnaw <nardidumessa@gmail.com>
2022-10-07 15:57:32 +0300

    For this

---
+++ dist/_nuxt/static/1665147423/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fservice","\u002F","\u002Fagricultural","\u002FAnalytical","\u002Fabout","\u002Fcontact","\u002FAluminum","\u002Fequipments","\u002Fproduct","\u002Feducation","\u002Flabratory"]})
