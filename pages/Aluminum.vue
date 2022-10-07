ead6a76d3e8578355cc387cfdc2bf6e0bb68d158
nardos atnaw <nardidumessa@gmail.com>
2022-10-07 14:29:37 +0300

    delopy try

--- pages/Aluminum.vue
+++ pages/Aluminum.vue
@@ -11,7 +11,7 @@
       <div class="container">
         <div class="category">
           <h3 class="category-title">
-            samples of aluminium doors and windows constructed by the company
+           This are samples of aluminium doors and windows constructed by the company
             (moretech)
           </h3>
           <div class="wrapper">
